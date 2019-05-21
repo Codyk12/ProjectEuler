b3623c0616323e96eac71d1ab3f8cd3496d80c97
Codyk12 <codac@live.com>
2019-05-21 12:43:56 -0400

    Problem 17

--- probs_11-20.jl
+++ probs_11-20.jl
@@ -129,7 +129,6 @@ function prob12()
     i = 1
     n = i
     factors = get_factors(n)
-    print(n, " ", factors, "\n")
     while factors < 500
         i += 1
         n += i
@@ -308,3 +307,84 @@ function prob16()
 end
 
 print(prob16())
+
+# ------------------------------------------------------------------------------
+
+function prob17(m, n)
+    """
+    Finds the sum of the letters of number m to n
+    """
+    single = Dict(
+    '1' => 3,
+    '2' => 3,
+    '3' => 5,
+    '4' => 4,
+    '5' => 4,
+    '6' => 3,
+    '7' => 5,
+    '8' => 5,
+    '9' => 4)
+
+    double = Dict(
+    "10" => 3,
+    "11" => 6 ,
+    "12" => 6 ,
+    "13" => 8 ,
+    "14" => 8 ,
+    "15" => 7 ,
+    "16" => 7 ,
+    "17" => 9 ,
+    "18" => 8 ,
+    "19" => 8,
+    "20" => 6,
+    "30" => 6,
+    "40" => 5,
+    "50" => 5,
+    "60" => 5,
+    "70" => 7,
+    "80" => 6,
+    "90" => 6,)
+
+    hundred = 7
+    and = 3
+
+    sum = 0
+
+    for i = m : n
+        s = string(i)
+        n = length(s)
+        if (n == 1)
+            sum += single[s[1]]
+        elseif (n == 2)
+            if (s[1] == '1')
+                sum += double[s]
+            else
+                sum += double[s[1]*"0"]
+                if (s[2] != '0')
+                    sum += single[s[2]]
+                end
+            end
+        elseif (n == 3)
+            sum += single[s[1]] + hundred
+            if (s[2:3] != "00")
+                sum += and
+                if (s[2] == '0')
+                    sum += single[s[3]]
+                elseif (s[2] == '1')
+                    sum += double[s[2:3]]
+                else # second digit is not 0 or 1
+                    sum += double[s[2]*"0"]
+
+                    if (s[3] != '0')
+                        sum += single[s[3]]
+                    end
+                end
+            end
+        elseif n == 4
+            sum += 11
+        end
+    end
+    sum
+end
+
+print(prob17(1, 1000))
