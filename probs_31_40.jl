7895dc0b3c4cf1e7d043fc1b7b5709a5c03abd3e
Codyk12 <codac@live.com>
2019-06-07 14:39:46 -0400

    Clean up

--- probs_31_40.jl
+++ probs_31_40.jl
@@ -1,9 +1,19 @@
 
-function prob31(coins=[1,2,5,10,20,50,100,200])
+function prob31(coins=[1,2,5,10,20,50,100,200], target=200)
     """
     Detects the number of ways to make the largest coin given any number of smaller coins.
     """
-    matrix = zeros(8,200)
+    ways = zeros(Int, target+1)
+    ways[1] = 1
+
+    for c in coins
+        if c <= target
+            for i = c:target
+                ways[i+1] += ways[(i-c)+1]
+            end
+        end
+    end
+    println("Ways to calculate ", target, " given coins: ", coins, ": ", ways[end])
 end
 
 prob31()
