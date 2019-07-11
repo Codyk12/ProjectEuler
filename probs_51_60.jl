2dcb784bd9561c5175e7f45d96d14d5344075c55
Cody <cody.kesler@icloud.com>
2019-07-11 10:07:58 -0400

    Problem 55

--- probs_51_60.jl
+++ probs_51_60.jl
@@ -28,3 +28,63 @@ end
 
 
 @time prob52()
+
+
+#-------------------------------------------------------------------------------
+using Combinatorics
+
+function prob53()
+    """
+        Number of binomial 1 < n < 100 above 1 million
+    """
+    cnt = 0
+    for n = 23:100
+        for r = 1:n
+            if binomial(BigInt(n), r) > 1000000
+                cnt += 1
+            end
+        end
+    end
+    println("Over 1 million: ", cnt)
+end
+
+@time prob53()
+
+#-------------------------------------------------------------------------------
+
+function ispalendrome(n::Integer)
+    """
+    Checks if number is palendrome
+    """
+    n = string(n)
+    for i = 1:Int(ceil(length(n)/2))
+        if n[i] != n[end-i+1]
+            return false
+        end
+    end
+    true
+end
+
+ispalendrome(132)
+
+function prob55()
+    """
+        Lychrel numbers that are below ten-thousand
+    """
+    cnt = 0
+    for i = 10:10000
+        num = BigInt(i)
+        is_pal = false
+        for j = 1:50
+            num += parse(BigInt, reverse(string(num)))
+            if ispalendrome(num)
+                is_pal = true
+                break
+            end
+        end
+        cnt += !is_pal ? 1 : 0
+    end
+    println(cnt, " Lychrel numbers are below ten-thousand")
+end
+
+@time prob55()
