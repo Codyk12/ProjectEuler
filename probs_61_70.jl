87df2c302a64a8c35ca0921987be853eb365a01e
Cody <cody.kesler@icloud.com>
2019-07-22 11:20:49 -0400

    Prob 67

--- probs_61_70.jl
+++ probs_61_70.jl
@@ -17,3 +17,25 @@ function prob63()
 end
 
 @time prob63()
+
+
+#-------------------------------------------------------------------------------
+
+
+function prob67(file="p067_triangle.txt")
+    """
+    Max path from top to bottom of given triangle file
+    """
+    triangle = split(read(open(file), String), "\n")
+    s = [[parse(Int, c) for c in split(row, " ")] for row in triangle[1:end-1]]
+
+    for i in Iterators.reverse(1:length(s)-1)
+        for j = 1:length(s[i])
+            s[i][j] += max(s[i+1][j], s[i+1][j+1])
+        end
+    end
+
+    println("Max path value: ", s[1][1])
+end
+
+prob67()
